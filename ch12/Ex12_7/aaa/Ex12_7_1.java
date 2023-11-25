fe60542366f9111ba05a98c4a0be70c8ced14a28
Xinyu320 <145210282+Xinyu320@users.noreply.github.com>
2023-11-26 04:01:32 +0800

    Add files via upload

---
+++ ch12/Ex12_7/aaa/Ex12_7_1.java
@@ -0,0 +1,9 @@
+package Ex12_7.aaa;
+import Ex12_7.aaa.bbb.Average;
+
+public class Ex12_7_1 {
+    public static void main(String[] args) {
+        Average a1 = new Average(10);
+        System.out.println("avg=" + a1.averg());
+    }
+}
